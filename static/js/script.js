d584c8a3da3d88ee0d2048e482f1e303a7ede281
Arvind-4 <lordarvindthegamer@gmail.com>
2021-10-15 21:02:04 +0530

    Changed Templates

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,16 @@
 document.getElementById('content').focus()
 var content = document.getElementById('content').value
-content.defaultValue = ''
-var final_content = content.replaceAll(' ', '')
-content.innerHTML = final_content
-
-const copy = document.getElementById("copy-button");
-const selection = window.getSelection();
-const range = document.createRange();
+const copy = document.getElementById("copy-button")
 const text = document.getElementById("result")
-var clear = document.getElementById('clear-button')
+content.defaultValue = ''
 
-
-function disable_button() {
-    if (text.value.length === 0) {
-        clear.disabled = true
-        copy.disabled = true
-    } else {
-        copy.onclick = function () {
-            text.select()
-            document.execCommand('copy')
-            copy.innerText = 'copied!'
-            window.getSelection().removeAllRanges()
-        }
-        clear.onclick = function () {
-            console.log('Clear')
-            text.value = ''
-        }
-    }
+function copy_all() {
+    text.select()
+    document.execCommand('copy')
+    copy.innerText = 'copied!'
+    window.getSelection().removeAllRanges()
 }
 
-//
-// document.getElementById('submit-button').addEventListener('click', function () {
-//     document.onreadystatechange = function () {
-//         if (document.readyState !== "complete") {
-//             document.querySelector("body").style.visibility = "none";
-//             document.querySelector("#loader").style.visibility = "none";
-//         } else {
-//             document.querySelector("#loader").style.display = "none";
-//             document.querySelector("body").style.visibility = "none";
-//         }
-//     };
-// })
+function clear_all() {
+    text.value = ''
+}
